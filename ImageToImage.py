4b77b6e501972fd082403ef1a5367229f203011e
Mithun <92502966+mithun02@users.noreply.github.com>
2023-09-25 13:16:47 +0530

    Add files via upload

---
+++ ImageToImage.py
@@ -0,0 +1,23 @@
+import PIL
+import requests
+import torch
+from diffusers import StableDiffusionInstructPix2PixPipeline, EulerAncestralDiscreteScheduler
+import matplotlib.pyplot as plt
+
+
+model_id = "timbrooks/instruct-pix2pix"
+pipe = StableDiffusionInstructPix2PixPipeline.from_pretrained(model_id, torch_dtype=torch.float16, safety_checker=None)
+pipe.to("cuda")
+pipe.scheduler = EulerAncestralDiscreteScheduler.from_config(pipe.scheduler.config)
+
+url = "https://images.unsplash.com/photo-1542909168-82c3e7fdca5c?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=imansyah-muhamad-putera-n4KewLKFOZw-unsplash.jpg"
+def download_image(url):
+    image = PIL.Image.open(requests.get(url, stream=True).raw)
+    image = PIL.ImageOps.exif_transpose(image)
+    image = image.convert("RGB")
+    return image
+image = download_image(url)
+
+prompt = "turn him into animated cartoon for avatar"
+images = pipe(prompt, image=image, num_inference_steps=10, image_guidance_scale=1).images
+plt.imshow(images[0])
